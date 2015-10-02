b619ee8ed346521d7a2c381e2919849a574469cf
agaurav <ashishgaurav.iitd@gmail.com>
2015-10-02 14:01:01 +0530

    updating

--- setup.sh
+++ setup.sh
@@ -20,29 +20,35 @@
 set -o nounset                              # Treat unset variables as an error
 
 if [ -f /etc/arch-release ]; then
-	sudo pacman -S --needed git vim conky zsh clang llvm python-dev ctags screen python-pip
+	sudo pacman -Su --needed git vim conky zsh clang llvm python-dev ctags screen python-pip
 	
 fi
 
 pip install ansible
-mkdir -p ~/udata/local/fonts
 
+mkdir ~/udata
 mkdir ~/share
 cd ~/share
 
-git clone git@github.com:agaurav/dotstuff.git
-cd dotstuff
+git clone --recursive git@github.com:agaurav/dotstuff.git
 
+ln -s $HOME/share/dotstuff/bash/bash_alias $HOME/.bash_alias
+ln -s $HOME/share/dotstuff/bash/bashrc $HOME/.bashrc
+ln -s $HOME/share/dotstuff/zsh/zshrc $HOME/.zshrc
+ln -s $HOME/share/dotstuff/zsh/oh-my-zsh $HOME/.oh-my-zsh
+mkdir $HOME/.vim
+ln -s $HOME/share/dotstuff/vim/rcs $HOME/.vim/rcs
+ln -s $HOME/share/dotstuff/vim/skeleton $HOME/.vim/skeleton
+ln -s $HOME/share/dotstuff/vim/vimrc $HOME/.vimrc
 
-
-
-echo "install awesome pacthed fonts"
-
+source $HOME/.bashrc
 
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim +PluginInstall +qall
 vim +GoInstallBinaries +qall
 
-
+echo "changing shell to zsh"
+chsh -s `which zsh`
+echo "install awesome pacthed fonts u lazy fc"
 #cd ~/udata/local/fonts/ && git clone https://github.com/ryanoasis/nerd-fonts.git
 #cd ~/udata/local/ && git clone https://github.com/numixproject/numix-icon-theme-circle.git
